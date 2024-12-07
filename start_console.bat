58d215c92036675e910fd3a52a0247380fd9ca39
cornelcristea <cornelcristea97@gmail.com>
2024-12-07 20:04:52 +0200

    update console file

--- start_console.bat
+++ start_console.bat
@@ -12,14 +12,15 @@
 setlocal enabledelayedexpansion
 
     rem variables
-        set "root_dir=%cd%"
-        set "robot_src_dir=%root_dir%\src\robot"
-        set "controller_src_dir=%root_dir%\src\controller"
-        set "lib_dir=%root_dir%\lib"
-        set "arduino_cli_path=%root_dir%\tools\arduino-cli"
-        set "build_dir=%root_dir%\build"
-        set "config_yml=%root_dir%\config.yml"
-        set "path_7zip=%root_dir%\tools\7-zip"
+        set "workdir=%cd%"
+        set "robot_src_dir=%workdir%\src\robot"
+        set "controller_src_dir=%workdir%\src\controller"
+        set "lib_dir=%workdir%\lib"
+        set "arduino_cli_path=%workdir%\tools\arduino-cli"
+        set "build_dir=%workdir%\build"
+        set "config_yml=%workdir%\config.yml"
+        set "path_7zip=%workdir%\tools\7-zip"
+        set "arduino_dir=%workdir%\arduino15\data\packages\arduino\hardware\%platform_name%\%platform_version%"
 
     rem update PATH variable
         set "PATH=%PATH%;%arduino_cli_path%;%path_7zip%"
@@ -30,33 +31,37 @@ setlocal enabledelayedexpansion
         set "board_name=nano"
  
     rem set option
+        call :InfoMessage
         if "%1" equ "" (
-            call :InfoMessage
+            set /p "user_input=Enter your choice: "
         ) else (
-            set "user_input=%1"            
+            set "user_input=%1"
+            echo Selected option: %user_input%           
         )
  
     rem install arduino packages
-        set "arduino_dir=%root_dir%\arduino15\data\packages\arduino\hardware\%platform_name%\%platform_version%"
-        if not exist %arduino_dir% call :InstallDependencies
+
 
     rem call specific function for user_input
         if "%user_input%"=="b"      call :Build %robot_src_dir% && call :Build %controller_src_dir%
         if "%user_input%"=="r"      call :Release
         if "%user_input%"=="br"     call :Build %robot_src_dir%
         if "%user_input%"=="bc"     call :Build %controller_src_dir%
+        if "%user_input%"=="a"      call :InstallArduino
+        if "%user_input%"=="l"      call :InstallLibraries
         :: if "%user_input%" equ "" echo Input parameter invalid && exit /b 1
         
 endlocal
 
-goto :eof 
+goto :eof
 
 :: display console info
 :InfoMessage
     echo.
     echo :::::::::::::::::::::::::::::::::::::::::::::::::::::
     echo ::                                                 ::
-    echo ::             Gesture Controlled Robot            ::
+    echo ::             GESTURE CONTROLLED ROBOT            ::
+    echo ::                Build Environment                ::
     echo ::                                                 ::
     echo :::::::::::::::::::::::::::::::::::::::::::::::::::::
     echo :: author  : Cornel Cristea                        ::
@@ -64,64 +69,72 @@ goto :eof
     echo :: version : 1.0.0                                 ::
     echo :::::::::::::::::::::::::::::::::::::::::::::::::::::
     echo.
-    echo This program is used to generate binary files.
-    echo The arduino dependencies and libraries will be 
-    echo downloaded and installed in project folder.
+    echo To generate binary files, the libraries and arduino
+    echo dependencies have to be installed first.
     echo.
     echo The available options are:
     echo.
     echo [b]    ^Build              ^Generate binary files
     echo [r]    ^Release            ^Create delivery archive
+    echo [l]    ^Install libraries  ^Compile only controller   
+    echo [a]    ^Install arduino    ^Install arduino packages   
     echo [br]   ^Build robot        ^Compile only robot
-    echo [bc]   ^Build controller   ^Compile only controller           
+    echo [bc]   ^Build controller   ^Compile only controller
     echo.
-    set /p "user_input=Enter your choice: "
-    goto :eof
+    exit /b
 
 
-:: install arduino and libraries
-:InstallDependencies
-    echo =====================================
-    echo Download Arduino dependencies
-    echo ===================================== 
-    arduino-cli core install arduino:%platform_name%@%platform_version% --config-file %config_yml% || call :ReturnError Arduino installation failed.
-    echo.
-    echo =====================================
-    echo Install project libraries
-    echo =====================================
+:: install arduino package
+:InstallArduino
+   call :PrintStep Download Arduino dependencies
+    arduino-cli core install arduino:%platform_name%@%platform_version% ^
+        --config-file %config_yml% || call :ReturnError Arduino installation failed.
+    exit /b
+    
+:: install project libraries
+:InstallLibraries
+    call :PrintStep Install project libraries
     for %%f in ("%lib_dir%\*.zip") do (
-        arduino-cli lib install --config-file %config_yml% --zip-path %%f || call :ReturnError Library "%%f" cannot be installed.
+        arduino-cli lib install ^
+            --config-file %config_yml% ^
+            --zip-path %%f || call :ReturnError Library "%%f" cannot be installed.
     )
-    goto :eof
+    exit /b
 
 :: build project
 :Build
     for %%f in ("%1") do set "src_file=%%~nxf"
-    echo =====================================
-    echo Build %src_file%
-    echo =====================================
-    arduino-cli compile -v -b arduino:%platform_name%:%board_name% %1 --build-property compiler.c.elf.flags="-Wl,-Map,%build_dir%\%src_file%.map" --config-file %config_yml% --output-dir %build_dir% || call :ReturnError Build failed.
-    goto :eof
+    call :PrintStep Build %src_file%
+
+    if not exist %arduino_dir% call :ReturnError Arduino dependencies not installed.
+    
+    if exist %build_dir% rmdir /s /q %build_dir%
+    mkdir %build_dir%
+
+    arduino-cli compile -v -b arduino:%platform_name%:%board_name% %1 ^
+        --build-property compiler.c.elf.flags="-Wl,-Map,%build_dir%\%src_file%.map" ^
+        --config-file %config_yml% ^
+        --log-file %workdir%\build_%src_file%.log ^
+        --output-dir %build_dir% || call :ReturnError Build failed.
+    exit /b
 
 :: create release zip archive
 :Release
-    echo =====================================
-    echo Create delivery package
-    echo =====================================
-    set "release_dir=%root_dir%\release"
+    call :PrintStep Create delivery package
+    set "release_dir=%workdir%\release"
     set "robot_release_dir=%release_dir%\robot"
     set "controller_release_dir=%release_dir%\controller"
-    set "delivery_zip=%root_dir%\binary_files.zip"
+    set "delivery_zip=%workdir%\binary_files.zip"
 
     rem create folders and copy files
         if exist %release_dir% rmdir /s /q %release_dir%
         mkdir %robot_release_dir% %controller_release_dir%
-
-        if not exist %build_dir% call start_console b
         
+        if not exist %build_dir% call :ReturnError Binary files not found. Build project first.
+
         if exist %delivery_zip% del /f /q %delivery_zip% 
 
-        echo Copy output files in specific folders.
+        echo Copy binary files in specific folders.
         for %%f in ("%build_dir%\*") do (
             set "filename=%%~nxf"
             echo "!filename!" | findstr /C:"controller" >nul
@@ -138,12 +151,20 @@ goto :eof
         7z a %delivery_zip% %release_dir% || call :ReturnError Archive %delivery_zip% could not be created.      
     
     rem delete release folder
-        if exist %release_dir% rmdir /s /q %release_dir%
+        rmdir /s /q %release_dir%
 
-    goto :eof
+    exit /b
  
+:: dispaly step message
+:PrintStep 
+    echo =====================================
+    echo %*
+    echo =====================================
+    exit /b
+
 :: display error message
 :ReturnError %*
     echo [ ERROR ] %*
+    pause
     exit 1
 
