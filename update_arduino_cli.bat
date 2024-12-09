891030cd9ecceaaa010e0c80ab792d7c918682aa
cornelcristea <cornelcristea97@gmail.com>
2024-12-09 18:32:07 +0200

    update scripts

--- update_arduino_cli.bat
+++ update_arduino_cli.bat
@@ -16,41 +16,36 @@ setlocal enabledelayedexpansion
         set "arduino_cli_path=%workdir%\tools\arduino-cli"
         set "PATH=%arduino_cli_path%;%PATH%"
 
-    rem get current version
-        set "current_version="
-        for /f "tokens=3 delims=: " %%v in ('arduino-cli version 2^>nul') do set "version_string=%%v"
-        for /f "tokens=1-3 delims=." %%a in ("!version_string!") do set "current_version=%%a.%%b.%%c"
-
     rem get version from input
+        call :InfoMessage
         if "%1" equ "" ( 
-            call :InfoMessage %current_version%
             set /p "ARDUINO_CLI_VERSION=Enter the new version: "            
         ) else (
             set "ARDUINO_CLI_VERSION=%1" 
         )
 
     rem check if new version is already installed
+        set "current_version="
+        for /f "tokens=3 delims=: " %%v in ('arduino-cli version 2^>nul') do set "version_string=%%v"
+        for /f "tokens=1-3 delims=." %%a in ("!version_string!") do set "current_version=%%a.%%b.%%c"
         if "%ARDUINO_CLI_VERSION%" == "%current_version%" (
-            call :PrintInfo arduino-cli %ARDUINO_CLI_VERSION% already installed.
-            exit /b 0
+            echo arduino-cli %ARDUINO_CLI_VERSION% already installed.
+            goto :eof
         )
 
     rem download new version
         set "arduino_cli_url=https://downloads.arduino.cc/arduino-cli/arduino-cli_%ARDUINO_CLI_VERSION%_Windows_64bit.zip"
         set "arduino_cli_zip=%arduino_cli_path%\arduino-cli_%ARDUINO_CLI_VERSION%_Windows_64bit.zip"
 
-        powershell -Command "Invoke-WebRequest -Uri \"%arduino_cli_url%\" -OutFile \"%arduino_cli_zip%\"" || call :ReturnError %ARDUINO_CLI_VERSION% not found
+        powershell -Command "Invoke-WebRequest -Uri \"%arduino_cli_url%\" -OutFile \"%arduino_cli_zip%\"" || call :ReturnError arduino-cli %ARDUINO_CLI_VERSION% not found
         powershell -Command "Expand-Archive -Path %arduino_cli_zip% -DestinationPath %arduino_cli_path% -Force"
-        
-        :: delete downloaded archive after it was extracted
-        if exist %arduino_cli_zip% del /F /Q %arduino_cli_zip%
 
-        call :PrintInfo arduino-cli %ARDUINO_CLI_VERSION% has been installed.
-        exit /b 0
+        :: delete downloaded archive after it was extracted
+        if exist %arduino_cli_zip% del /f /q %arduino_cli_zip%
+        echo arduino-cli %ARDUINO_CLI_VERSION% has been installed
 
 endlocal
-
-exit /b
+goto :eof
 
 :InfoMessage 
     echo.
@@ -67,15 +62,8 @@ exit /b
     echo.
     echo This program is used to update Arduino CLI tool        
     echo.
-    echo Current version : %1
-    echo.
-    echo Enter the new version (e.g: 1.0.1) 
-    exit /b 0
+    exit /b
 
 :ReturnError 
     echo [ ERROR ] %*
     exit 1
-
-:PrintInfo
-    echo [ INFO ] %*
-    exit /b
